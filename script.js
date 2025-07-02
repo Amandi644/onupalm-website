f06fa0827cd61fb6d5518a5502ca8246c7fed196
Amandi644 <okukugeorgi@yahoo.com>
2025-07-02 17:52:32 +0200

    Add files via upload
    Onu-palm mill in Ideato South.
    visite us for your Palm seeds productions

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+const cartQuantity  = document.getElementById("cartQuantity");
+
+function button() {
+console.log("value subcribe!");
+alert(`Subcribe the on your Website`);
+}
+button();
+button();
+
+let joinThem = "";
+for(i = 0; i < 15; ++i){
+console.log(i);
+alert(i,"Were is he coming from");
+}
+
+
+let hour = "";
+if (12 < 18){
+  console.log ("That is true");
+}
+else if (34 < 23){
+   console.log ("Wir werden da sein");
+
+}
+/*
+
+
